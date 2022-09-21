8ee9bcd6f2e9fd0359873dbbcf8cd841cee29220
Addarsh <addarsh.cherokie@gmail.com>
2022-09-21 15:34:34 -0700

    Added initial views

---
+++ InitialVC.swift
@@ -0,0 +1,26 @@
+//
+//  InitialVC.swift
+//  reachout_ios
+//
+//  Created by Addarsh Chandrasekar on 9/21/22.
+//
+
+import UIKit
+
+class InitialVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func login() {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "TabsVC")
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
+
+}
---
+++ TabsVC.swift
@@ -0,0 +1,18 @@
+//
+//  TabsVC.swift
+//  reachout_ios
+//
+//  Created by Addarsh Chandrasekar on 9/21/22.
+//
+
+import UIKit
+
+class TabsVC: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
